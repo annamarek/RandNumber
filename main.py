56366842585aceb88792614c38dc6c346036bb01
Anna Stoliarova <anastolarova61@gmail.com>
2024-12-18 11:37:25 +0300

    Added diaposon

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from random import random
 from tkinter import *
 import random
+from tkinter.ttk import Combobox
+
+
 def generate():
-    n=random.randint(0,1000)
-    label.config(text=n)
+    minNum = int(minCombobox.get())
+    maxNum = int(maxCombobox.get())
+    if minNum<maxNum:
+        n=random.randint(minNum,maxNum)
+        label.config(text=n)
+    else:
+        label.config(text="Error")
 
 root = Tk()
 root.title("Generate random number")
 root.geometry("200x200")
 label = Label(text="-")
 label.pack()
+minCombobox=Combobox(root,values=[str(i) for i in range(1,101)])
+minCombobox.set("1")
+minCombobox.pack()
+maxCombobox=Combobox(root,values=[str(i) for i in range(1,101)])
+maxCombobox.set("100")
+maxCombobox.pack()
 button = Button(text="Generate", command=generate)
 button.pack()
 
